a35b81e9c966de48bfee5bb0b924554585e7c7a6
prashantkit <prashantkit@gmail.com>
2021-03-01 16:20:47 -0600

    first commit

---
+++ src/gitdemo.java
@@ -0,0 +1,13 @@
+
+public class gitdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hi");
+System.out.println("hi1");
+System.out.println("hi2");
+System.out.println("hi3");
+System.out.println("posting");
+	}
+
+}
